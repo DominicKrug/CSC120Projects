0249eeb32d57fa102cd5138e88f77da8460181bc
DominicKrug <Domkrug123456@gmail.com>
2024-09-04 21:46:41 -0400

    Add files via upload

---
+++ LabAssignment1.py
@@ -0,0 +1,23 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+# LabAssignment 1
+# Dominic Krug
+# Python and Github
+
+print("I am a programmer")
+print("I am leanring Python")
+print("Github is cool")
